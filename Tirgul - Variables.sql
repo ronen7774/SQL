a3367bcbfdccccf320abb31ba517e89450b7ea99
Ronen <you@example.com>
2025-08-02 13:25:07 +0300

    Fix a letter in  the  code

--- Tirgul - Variables.sql
+++ Tirgul - Variables.sql
@@ -221,7 +221,7 @@ else begin select 'The price is beloew market value' end
 go
 Declare @prod_name varchar (15),
 @price money
-SET @prod_name = 'Chang'
+SET @prod_name = 'Change'
 
 SELECT @price = UnitPrice
 FROM Products
